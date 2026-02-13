ae264b1ab9f4083eb285ca2d674003900cc9ba18
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-12 19:31:29 -0800

    Register DeathListener and commands in onEnable

--- src/main/java/com/hiddentest/DeathListener.java
+++ src/main/java/com/hiddentest/DeathListener.java
@@ -1,1 +1,24 @@
+package com.hiddentest;
 
+import com.hiddentest.reveal.RevealCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HiddenTest extends JavaPlugin {
+
+    private static HiddenTest instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        getServer().getPluginManager().registerEvents(new ProfileManager(), this);
+        getServer().getPluginManager().registerEvents(new DeathListener(), this);
+
+        getCommand("reveal").setExecutor(new RevealCommand());
+        getCommand("hide").setExecutor(new RevealCommand());
+    }
+
+    public static HiddenTest getInstance() {
+        return instance;
+    }
+}
