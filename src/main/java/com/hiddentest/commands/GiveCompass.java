90b302cd00e588950963095f720b6a0db2a50acd
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-15 18:03:06 -0800

    Implement GiveCompass command for players

--- src/main/java/com/hiddentest/commands/GiveCompass.java
+++ src/main/java/com/hiddentest/commands/GiveCompass.java
@@ -1,1 +1,40 @@
+package com.hiddentest.commands;
 
+import com.hiddentest.items.DetectivesCompass;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class GiveCompassCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!sender.hasPermission("hiddentest.givecompass")) {
+            sender.sendMessage(ChatColor.RED + "You don't have permission.");
+            return true;
+        }
+
+        if (args.length != 1) {
+            sender.sendMessage(ChatColor.RED + "Usage: /givecompass <player>");
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            sender.sendMessage(ChatColor.RED + "Player not found.");
+            return true;
+        }
+
+        ItemStack compass = DetectivesCompass.createDetectivesCompass();
+        target.getInventory().addItem(compass);
+
+        sender.sendMessage(ChatColor.GREEN + "Gave Detective’s Compass to " + target.getName());
+        target.sendMessage(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "You received a Detective’s Compass!");
+
+        return true;
+    }
+}
