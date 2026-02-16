8954bedd3b375789be27ad5c94daa0596bba445c
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-16 15:04:26 -0800

    Update HiddenTest.java

--- src/main/java/com/hiddentest/HiddenTest.java
+++ src/main/java/com/hiddentest/HiddenTest.java
@@ -1,6 +1,7 @@
 package com.hiddentest;
 
 import com.hiddentest.commands.GiveCompassCommand;
+import com.hiddentest.commands.GiveBellCommand;
 import com.hiddentest.items.DetectivesCompass;
 import com.hiddentest.reveal.HourlyReveal;
 import com.hiddentest.reveal.RevealCommand;
@@ -25,10 +26,14 @@ public final class HiddenTest extends JavaPlugin {
         // 🔥 Detective Compass listener
         getServer().getPluginManager().registerEvents(new DetectivesCompass(this), this);
 
+        // 🔔 Bell of Truth listener
+        getServer().getPluginManager().registerEvents(new BellOfTruth(this), this);
+
         // Commands
         getCommand("reveal").setExecutor(new RevealCommand());
         getCommand("hide").setExecutor(new RevealCommand());
         getCommand("givecompass").setExecutor(new GiveCompassCommand());
+        getCommand("givebell").setExecutor(new GiveBellCommand(this));
 
         // Start RevealManager timer system
         RevealManager.init();
