24371d72a587d0040009df0d50ffe32113ce2b0c
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-21 21:33:29 -0800

    Update HiddenTest.java

--- src/main/java/com/hiddentest/HiddenTest.java
+++ src/main/java/com/hiddentest/HiddenTest.java
@@ -6,6 +6,7 @@ import com.hiddentest.hearts.EggHeart;
 import com.hiddentest.items.DetectivesCompass;
 import com.hiddentest.items.DragonEgg;
 import com.hiddentest.items.BellOfTruth;
+import com.hiddentest.items.BookOfNames;
 import com.hiddentest.mobs.UnknownChicken;
 import com.hiddentest.reveal.HourlyReveal;
 import com.hiddentest.reveal.RevealCommand;
@@ -59,6 +60,7 @@ public final class HiddenTest extends JavaPlugin {
         getCommand("hide").setExecutor(new RevealCommand());
         getCommand("givecompass").setExecutor(new GiveCompassCommand());
         getCommand("givebell").setExecutor(new GiveBellCommand(this));
+        getCommand("givebook").setExecutor(new BookOfNames()); // ✅ Added
 
         // =========================
         // INIT REVEAL MANAGER
