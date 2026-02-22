a5e17e823417d29114f3ec5cb163ec6a22fd2cbd
dareymorales55-ux <dareymorales55@gmail.com>
2026-02-21 17:02:17 -0800

    Update HiddenTest.java

--- src/main/java/com/hiddentest/HiddenTest.java
+++ src/main/java/com/hiddentest/HiddenTest.java
@@ -2,11 +2,9 @@ package com.hiddentest;
 
 import com.hiddentest.commands.GiveCompassCommand;
 import com.hiddentest.commands.GiveBellCommand;
-import com.hiddentest.commands.GiveStarCommand;
 import com.hiddentest.hearts.EggHeart;
 import com.hiddentest.items.DetectivesCompass;
 import com.hiddentest.items.DragonEgg;
-import com.hiddentest.items.ShadowStar;
 import com.hiddentest.reveal.HourlyReveal;
 import com.hiddentest.reveal.RevealCommand;
 import com.hiddentest.reveal.RevealManager;
@@ -31,7 +29,6 @@ public final class HiddenTest extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new DetectivesCompass(this), this);
         getServer().getPluginManager().registerEvents(new BellOfTruth(this), this);
         getServer().getPluginManager().registerEvents(new DragonEgg(this), this);
-        getServer().getPluginManager().registerEvents(new ShadowStar(this), this); // ⭐ REGISTERED
 
         // Hearts
         getServer().getPluginManager().registerEvents(new EggHeart(this), this);
@@ -41,7 +38,6 @@ public final class HiddenTest extends JavaPlugin {
         getCommand("hide").setExecutor(new RevealCommand());
         getCommand("givecompass").setExecutor(new GiveCompassCommand());
         getCommand("givebell").setExecutor(new GiveBellCommand(this));
-        getCommand("givestar").setExecutor(new GiveStarCommand()); // ⭐ REGISTERED
 
         RevealManager.init();
     }
