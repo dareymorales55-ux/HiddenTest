e6bd32ce87b8992eb9a3c1258164adce694ff1ee
dareymorales55-ux <dareymorales55@gmail.com>
2026-05-19 20:45:33 -0700

    Rename UnknownTotemCommand to GiveTotemCommand

--- src/main/java/com/hiddentest/commands/GiveTotemCommand.java
+++ src/main/java/com/hiddentest/commands/GiveTotemCommand.java
@@ -10,7 +10,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class UnknownTotemCommand implements CommandExecutor {
+public class GiveTotemCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(
